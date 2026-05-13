2a9080467e6f0abd1dbb5cd3a7eacf415197bb54
Philip Grenon <pdgrenon@gmail.com>
2026-05-13 18:07:25 -0400

    Fix a typo in the vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/guards-ledger'
+  base: '/guards-ledger/',
   test: {
     environment: 'jsdom',
     globals: true,
